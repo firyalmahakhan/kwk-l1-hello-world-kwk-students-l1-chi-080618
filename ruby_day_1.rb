adb939f9c00849b24b7901f63c1f015b4280bc05
firyalmahakhan <firyalmahak@gmail.com>
2018-08-06 19:44:01 +0000

    automatically backed up by learn

--- ruby_day_1.rb
+++ ruby_day_1.rb
@@ -2,4 +2,10 @@
 # puts "I love Koding!"
 
 #print "Yay Ruby!
-"
+# print "I love Koding!
+
+# "Yay Ruby!" 
+# "I love Koding!"
+
+puts "Hello World".downcase.length 
+puts "Hello World.length.downcase""
